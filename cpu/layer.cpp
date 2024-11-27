c92ee1108a5e6b5b25ce00eaaab8a764ae67cd9e
chihyanl <chihyanl@andrew.cmu.edu>
2024-11-27 00:34:30 -0500

    cpu version

---
+++ cpu/layer.cpp
@@ -0,0 +1,235 @@
+#include <stdlib.h>
+#include <cmath>
+#include <stdio.h>
+
+#include "layer.h"
+
+
+float randomFloat(float a, float b) {
+  float random_value = (float)rand() / RAND_MAX;
+  return random_value * (b - a) + a;
+}
+
+void printTest(float input, int in_channels) {
+  if (in_channels == 6 && (std::isnan(input) || std::abs(input)>1)) printf("%f ", input);
+}
+
+Conv::Conv(int in_channels, int out_channels, int height, int width, int kernel_h, int kernel_w, int stride, int pad)
+  : height(height),               // input height
+    width(width),                 // input width
+    in_channels(in_channels),     // number of input channels
+    out_channels(out_channels),   // number of output channels
+    kernel_h(kernel_h),           // filter height
+    kernel_w(kernel_w),           // filter width
+    stride(stride),
+    pad(pad),
+    height_out((height + 2 * pad - kernel_h) / stride + 1),
+    width_out((width + 2 * pad - kernel_w) / stride + 1) {
+
+  output = (float*)calloc(out_channels * height_out * width_out, sizeof(float));
+  bias = (float*)calloc(out_channels, sizeof(float));
+  weight = (float*)calloc(out_channels * in_channels * kernel_h * kernel_w, sizeof(float));
+
+  u_bias = (float*)calloc(out_channels, sizeof(float));
+  u_weight = (float*)calloc(out_channels * in_channels * kernel_h * kernel_w, sizeof(float));
+  gradient = (float*)calloc(out_channels * height_out * width_out, sizeof(float));
+  error = (float*)calloc(out_channels * height_out * width_out, sizeof(float));
+
+  printf("Conv=%dx%dx%d (%d->%d), Kernel=%dx%d, Stride=%d, Pad=%d, Weight=%d, Bias=%d\n", height_out, width_out, out_channels, in_channels*width*height, out_channels*height_out*width_out, kernel_h, kernel_w, stride, pad, out_channels*in_channels*kernel_h*kernel_w, out_channels);
+
+  float k = sqrt(1.0f / (in_channels * kernel_h * kernel_w));
+  for (int i = 0; i < out_channels * in_channels * kernel_h * kernel_w; i++) {
+    weight[i] = randomFloat(-k, k);
+  }
+  for (int i = 0; i < out_channels; i++) {
+    bias[i] = randomFloat(-k, k);
+  }
+}
+
+Conv::~Conv() {
+  free(output);
+  free(bias);
+  free(weight);
+  free(error);
+  free(u_bias);
+  free(u_weight);
+  free(gradient);
+}
+
+void Conv::forward(float* input) {
+  for (int dst_channel = 0; dst_channel < out_channels; dst_channel++) {
+    int dstIdx_base = dst_channel * in_channels * kernel_h * kernel_w;
+    int outIdx_base = dst_channel * height_out * width_out;
+    for (int dst_y = 0; dst_y < height_out; dst_y++) {
+      int src_y = stride * dst_y - pad;
+      int outIdx_base2 = dst_y * width_out;
+      for (int dst_x = 0; dst_x < width_out; dst_x++) {
+        int src_x = stride * dst_x - pad;
+        int outIdx = outIdx_base + outIdx_base2 + dst_x;
+        float sum = bias[dst_channel];
+        for (int src_channel = 0; src_channel < in_channels; src_channel++) {
+          int dstIdx_base2 = src_channel * kernel_h * kernel_w;
+          int srcIdx_base = src_channel * width * height;
+          for (int dy = 0; dy < kernel_h; dy++) {
+            int y = src_y + dy;
+            if (y >= 0 && y < height) {
+              int srcIdx = srcIdx_base + y * width;
+              int dstIdx = dstIdx_base + dstIdx_base2 + dy * kernel_w;
+              for (int dx = 0; dx < kernel_w; dx++) {
+                int x = src_x + dx;
+                if (x >= 0 && x < width) {
+                  sum += input[srcIdx+x] * weight[dstIdx+dx];
+                }
+              }
+            }
+          }
+        }
+        sum = (sum > 0) ? sum : 0;  // relu
+        output[outIdx] = sum;
+        gradient[outIdx] = (sum > 0) ? 1 : 0;
+      }
+    }
+  }
+}
+
+void Conv::backward(float* input, float* src_error) {
+  if (src_error != NULL) {
+    for (int i = 0; i < in_channels * width * height; i++) {
+      src_error[i] = 0;
+    }
+  }
+
+  for (int dst_channel = 0; dst_channel < out_channels; dst_channel++) {
+    int dstIdx_base = dst_channel * in_channels * kernel_h * kernel_w;
+    int outIdx_base = dst_channel * height_out * width_out;
+    for (int dst_y = 0; dst_y < height_out; dst_y++) {
+      int src_y = stride * dst_y - pad;
+      int outIdx_base2 = dst_y * width_out;
+      for (int dst_x = 0; dst_x < width_out; dst_x++) {
+        int src_x = stride * dst_x - pad;
+        int outIdx = outIdx_base + outIdx_base2 + dst_x;
+        float dnet = error[outIdx] * gradient[outIdx];
+        u_bias[dst_channel] += dnet;
+        for (int src_channel = 0; src_channel < in_channels; src_channel++) {
+          int dstIdx_base2 = src_channel * kernel_h * kernel_w;
+          int srcIdx_base = src_channel * width * height;
+          for (int dy = 0; dy < kernel_h; dy++) {
+            int y = src_y + dy;
+            if (y >= 0 && y < height) {
+              int srcIdx = srcIdx_base + y * width;
+              int dstIdx = dstIdx_base + dstIdx_base2 + dy * kernel_h;
+              for (int dx = 0; dx < kernel_w; dx++) {
+                int x = src_x + dx;
+                if (x >= 0 && x < width) {
+                  if (src_error != NULL) {
+                    src_error[srcIdx+x] += weight[dstIdx+dx] * dnet;
+                  }
+                  u_weight[dstIdx+dx] += dnet * input[srcIdx+x];
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void Conv::update(float rate) {
+  for (int i = 0; i < out_channels; i++) {
+    bias[i] -= rate * u_bias[i];
+    u_bias[i] = 0;
+  }
+  for (int i = 0; i < out_channels * in_channels * kernel_h * kernel_w; i++) {
+    weight[i] -= rate * u_weight[i];
+    u_weight[i] = 0;
+  }
+}
+
+
+Linear::Linear(int in_channels, int out_channels)
+  : in_channels(in_channels),
+    out_channels(out_channels) {
+
+  output = (float*)calloc(out_channels, sizeof(float));
+  bias = (float*)calloc(out_channels, sizeof(float));
+  weight = (float*)calloc(in_channels * out_channels, sizeof(float));
+
+  error = (float*)calloc(out_channels, sizeof(float));
+  u_bias = (float*)calloc(out_channels, sizeof(float));
+  u_weight = (float*)calloc(in_channels * out_channels, sizeof(float));
+  gradient = (float*)calloc(out_channels, sizeof(float));
+  
+  printf("Linear=%d->%d\n", in_channels, out_channels);
+
+  float k = 1.0f / in_channels;
+  for (int i = 0; i < out_channels * in_channels; i++) {
+    weight[i] = randomFloat(-k, k);
+  }
+  for (int i = 0; i < out_channels; i++) {
+    bias[i] = randomFloat(-k, k);
+  }
+}
+
+Linear::~Linear() {
+  free(output);
+  free(bias);
+  free(weight);
+  free(error);
+  free(u_bias);
+  free(u_weight);
+  free(gradient);
+}
+
+void Linear::forward(float* input) {
+  for (int i = 0; i < out_channels; i++) {
+    float x = bias[i];
+    for (int j = 0; j < in_channels; j++) {
+      x += input[j] * weight[i*in_channels+j];
+    }
+    output[i] = x;
+  }
+   
+  //softmax
+  float max = output[0];
+  for (int i = 1; i < out_channels; i++) {
+    if (max < output[i]) {
+      max = output[i];
+    }
+  }
+  float sum = 0.0001;
+  for (int i = 0; i < out_channels; i++) {
+    output[i] = exp(output[i] - max);
+    sum += output[i];
+  }
+  for (int i = 0; i < out_channels; i++) {
+    output[i] /= sum;
+  }
+}
+
+void Linear::backward(float* input, float* src_error) {
+  for (int i = 0; i < in_channels; i++) {
+    src_error[i] = 0;
+  }
+
+  for (int i = 0; i < out_channels; i++) {
+    gradient[i] = output[i] + error[i];
+    for (int j = 0; j < in_channels; j++) {
+      src_error[j] += weight[i*in_channels+j] * gradient[i];
+      u_weight[i*in_channels+j] += gradient[i] * input[j];
+    }
+    u_bias[i] += gradient[i];
+  }
+}
+
+void Linear::update(float rate) {
+  for (int i = 0; i < out_channels; i++) {
+    bias[i] -= rate * u_bias[i];
+    error[i] = 0;
+    u_bias[i] = 0;
+    for (int j = 0; j < in_channels; j++) {
+      weight[i*in_channels+j] -= rate * u_weight[i*in_channels+j];
+      u_weight[i*in_channels+j] = 0;
+    }
+  }
+}
---
+++ cpu/main.cpp
@@ -0,0 +1,157 @@
+
+#include "dataset.h"
+#include "layer.h"
+#include "CycleTimer.h"
+
+#define EPOCH 2
+#define LR 0.005
+#define BATCH 1
+
+int main(int argc, char** argv) {
+  int train_count, test_count, epoch_count;
+
+  srand(0);
+
+  if (argc == 1) {
+    train_count = TRAIN_SIZE;
+    test_count = TEST_SIZE;
+    epoch_count = EPOCH;
+  } else if (argc == 3) {
+    train_count = stoi(argv[1]);
+    test_count = stoi(argv[2]);
+    epoch_count = EPOCH;
+  } else if (argc == 4) {
+    train_count = stoi(argv[1]);
+    test_count = stoi(argv[2]);
+    epoch_count = stoi(argv[3]);
+  } else {
+    printf("Error, incorrect argument count\n");
+    return 1;
+  }
+  for (int i = 0; i < 50; i++) printf("-");
+  printf("\nTraining Samples: %d\nTesting Samples: %d\nEpochs: %d\nLearning Rate: %g\nBatch Size: %d\n", train_count, test_count, epoch_count, LR, BATCH);
+  for (int i = 0; i < 50; i++) printf("-");
+  printf("\nInput=%dx%dx1\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+
+  // Create model
+  // Conv(int in_channels, int out_channels, int height, int width, int kernel_h, int kernel_w, int stride, int pad)
+  // Linear(int in_channels, int out_channels)
+  Conv* l1 = new Conv(1, 6, 28, 28, 5, 5, 1, 2);  // 28x28x1 -> 28x28x6
+  Conv* l2 = new Conv(6, 16, 28, 28, 4, 4, 2, 0);  // 28x28x6 -> 13x13x16
+  Conv* l3 = new Conv(16, 8, 13, 13, 3, 3, 1, 1);  // 13x13x16 -> 13x13x8
+  Conv* l4 = new Conv(8, 4, 13, 13, 3, 3, 1, 0);  // 13x13x8 -> 11x11x4
+  Linear* l5 = new Linear(484, 10);
+
+  // train
+  for (int i = 0; i < 50; i++) printf("-");
+  printf("\nTRAINING\n");
+  {
+    int err = 0;
+    float* train_data = (float*)malloc(sizeof(float) * TRAIN_SIZE * IMAGE_HEIGHT * IMAGE_WIDTH);
+    int* train_label = (int*)malloc(sizeof(int) * TRAIN_SIZE);
+  
+    err += get_MNIST_data("../dataset/train-images-idx3-ubyte", train_data, true);
+    err += get_MNIST_label("../dataset/train-labels-idx1-ubyte", train_label, true);
+
+    if (err) {
+      return 1;
+    }
+  
+    for (int i = 1; i <= epoch_count; i++) {
+      double startTime = CycleTimer::currentSeconds();
+      int correct = 0;
+      for (int j = 0; j < train_count; j++) {
+        // forward
+        l1->forward(&train_data[j*IMAGE_HEIGHT*IMAGE_WIDTH]);
+        l2->forward(l1->output);
+        l3->forward(l2->output);
+        l4->forward(l3->output);
+        l5->forward(l4->output);
+
+        int y = train_label[j];
+        int pred = 0;
+        float local_max = l5->output[0];
+        l5->error[y] -= 1;
+        for (int k = 1; k < 10; k++) {
+          if (l5->output[k] > local_max) {
+            pred = k;
+            local_max = l5->output[k];
+          }
+        }
+        if (pred == y) {
+          correct++;
+        }
+      
+        // backward
+        l5->backward(l4->output, l4->error);
+        l4->backward(l3->output, l3->error);
+        l3->backward(l2->output, l2->error);
+        l2->backward(l1->output, l1->error);
+        l1->backward(&train_data[j*IMAGE_HEIGHT*IMAGE_WIDTH], NULL);
+
+        // update
+        if (j % BATCH == 0) {
+          l1->update(LR / BATCH);
+          l2->update(LR / BATCH);
+          l3->update(LR / BATCH);
+          l4->update(LR / BATCH);
+          l5->update(LR / BATCH);
+        }
+      }
+      double endTime = CycleTimer::currentSeconds();
+      printf("%d/%d: Accuracy %.2f%%, Time %.5f s\n", i, epoch_count, (float)correct/train_count*100, endTime-startTime);
+    }
+
+    free(train_data);
+    free(train_label);
+  }
+
+  // test
+  for (int i = 0; i < 50; i++) printf("-");
+  printf("\nTESTING\n");
+  {
+    int err = 0;
+    float* test_data = (float*)malloc(sizeof(float) * TEST_SIZE * IMAGE_HEIGHT * IMAGE_WIDTH);
+    int* test_label = (int*)malloc(sizeof(int) * TEST_SIZE);
+  
+    err += get_MNIST_data("../dataset/t10k-images-idx3-ubyte", test_data, false);
+    err += get_MNIST_label("../dataset/t10k-labels-idx1-ubyte", test_label, false);
+
+    if (err) {
+      return 1;
+    }
+  
+    double startTime = CycleTimer::currentSeconds();
+    int correct = 0;
+    for (int j = 0; j < test_count; j++) {
+      // forward
+      l1->forward(&test_data[j*IMAGE_HEIGHT*IMAGE_WIDTH]);
+      l2->forward(l1->output);
+      l3->forward(l2->output);
+      l4->forward(l3->output);
+      l5->forward(l4->output);
+
+      int y = test_label[j];
+      int pred = 0;
+      float local_max = l5->output[0];
+      l5->error[y] -= 1;
+      for (int k = 1; k < 10; k++) {
+        if (l5->output[k] > local_max) {
+          pred = k;
+          local_max = l5->output[k];
+        }
+      }
+      if (pred == y) {
+        correct++;
+      }
+    }
+    double endTime = CycleTimer::currentSeconds();
+    printf("Accuracy %.2f%%, Time %.5f s\n", (float)correct/test_count*100, endTime-startTime);
+
+    free(test_data);
+    free(test_label);
+  }
+
+  return 0;
+}
